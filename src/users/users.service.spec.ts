0f2f98a1823dd44d4f5bace39fbe7af34bcb2464
Havimaki <dm.havimaki@gmail.com>
2022-07-07 12:02:47 -0400

    Add tests for user.service.ts

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 
@@ -8,11 +9,28 @@ describe('UsersService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService],
     }).compile();
-
     service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findOne', () => {
+    it('should return the User entity', async () => {
+      const res = await service.findOne(process.env.USER_NAME)
+
+      expect(res).toEqual({
+        id: 1,
+        password: process.env.USER_PASSWORD,
+        username: process.env.USER_NAME
+      })
+    });
+
+    it('should return nothing', async () => {
+      const res = await service.findOne('random')
+
+      expect(res).toEqual(undefined)
+    })
+  })
 });
